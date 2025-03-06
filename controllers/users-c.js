ae82a154308147c93fa5727c0f8dad10e372742e
Manuel Calderon <manuu.calderonn@gmail.com>
2025-03-06 18:03:47 -0500

    project 12, postman issue

---
+++ controllers/users-c.js
@@ -0,0 +1,18 @@
+const user = require("../models/user");
+const User = require("../models/user");
+
+const getUsers = (req, res) => {
+  User.find({})
+    .then((users) => res.send(users))
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).send({ message: err.message });
+    });
+};
+
+const createUser = (req, res) => {
+  const { name, avatar } = req.body;
+  console.log(name, avatar);
+};
+
+module.exports = { getUsers, createUser };
