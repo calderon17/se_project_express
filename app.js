53a52a5c1f4ad13cc762a83a0af43dd2b0abe82d
Manuel Calderon <manuu.calderonn@gmail.com>
2025-05-13 08:44:40 -0400

    sprint 15, updating changes for cloud deployment

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors"); // before import controller/users
+const cors = require("cors");
+const { errors } = require("celebrate"); //theory
+const { requestLogger, errorLogger } = require("./middlewares/logger"); //theory // before import controller/users
 
 const { createUser, login } = require("./controllers/users");
 const routes = require("./routes");
 
 const app = express(); // this goes frist over all
 const { PORT = 3001 } = process.env;
+
+// const errorHandler = require("./middlewares/error-handler"); //theory
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db")
@@ -18,11 +22,28 @@ mongoose
 app.use(cors());
 app.use(express.json()); // needs to be 1st or before routes
 
+app.use(requestLogger); //theory
+
 app.post("/signin", login);
 app.post("/signup", createUser);
 
 app.use(routes); // needs to be after routes for proper order
 
+app.use(errorLogger); //theory
+app.use(errors()); // celebrate error handler //theory
+// app.use(errorHandler);  //theory
+
+// Custom error handler
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+  res.status(statusCode).send({
+    message: statusCode === 500 ? "An error occurred on the server" : message,
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`listening on port 3001`);
 });
+
+// ... all other app.use() statements
+// app.use(errorHandler);
---
+++ middlewares/validation.js
@@ -0,0 +1,80 @@
+const { Joi, celebrate } = require("celebrate");
+const validator = require("validator");
+
+const validateURL = (value, helpers) => {
+  if (validator.isURL(value)) {
+    return value;
+  }
+  return helpers.error("string.uri");
+};
+const validateAuthentication = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email().messages({
+      "string.empty": 'The "email" field must be filled in',
+      "string.email": 'The "email" field must be a valid email',
+    }),
+    password: Joi.string().required().messages({
+      "string.empty": 'The "password" field must be filled in',
+    }),
+  }),
+});
+
+const validateUserInfo = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30).messages({
+      "string.empty": 'The "name" field must be filled in',
+      "string.min": 'The "name" field must be at least 2 characters',
+      "string.max": 'The "name" field must be less than 30 characters',
+    }),
+    avatar: Joi.string().required().uri().messages({
+      "string.empty": 'The "avatar" field must be filled in',
+      "string.uri": 'The "avatar" field must be a valid url',
+    }),
+    email: Joi.string().required().email().messages({
+      "string.empty": 'The "email" field must be filled in',
+      "string.email": 'The "email" field must be a valid email',
+    }),
+    password: Joi.string().required().messages({
+      "string.empty": 'The "password" field must be filled in',
+    }),
+  }),
+});
+
+const validateClothingItem = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30).messages({
+      "string.empty": 'The "name" field must be filled in',
+      "string.min": 'The "name" field must be at least 2 characters',
+      "string.max": 'The "name" field must be less than 30 characters',
+    }),
+    imageUrl: Joi.string().required().uri().messages({
+      "string.empty": 'The "imageUrl" field must be filled in',
+      "string.uri": 'the "imageUrl" field must be a valid url',
+    }),
+    weather: Joi.string().valid("hot", "warm", "cold").messages({
+      "any.only": 'The "weather" field must be either "hot", "warm", or "cold"',
+    }),
+  }),
+});
+
+const validateIds = celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().hex().length(24).messages({
+      "string.empty": 'The "userId" field must be filled in',
+      "string.hex": 'The "userId" field must be a hexadecimal value',
+      "string.length": 'The "userId" field must be 24 characters',
+    }),
+    itemId: Joi.string().hex().length(24).messages({
+      "string.empty": 'The "itemId" field must be filled in',
+      "string.hex": 'The "itemId" field must be a hexadecimal value',
+      "string.length": 'The "itemId" field must be 24 characters',
+    }),
+  }),
+});
+
+module.exports = {
+  validateAuthentication,
+  validateUserInfo,
+  validateClothingItem,
+  validateIds,
+};
